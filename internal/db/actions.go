cef51e4bada6859e71269ae0a45de0ed19b30925
md <49080343+opp@users.noreply.github.com>
2024-04-02 22:01:53 -0400

    embed all webui related files to single binary when compiling

--- internal/db/actions.go
+++ internal/db/actions.go
@@ -26,11 +26,10 @@ func Add(database *sql.DB, url, title, note, keywords, bGroup string, archive bo
 		logger.Error.Fatalln(err)
 	}
 
-	execResult, err := stmt.Exec(url, title, note, keywords, bGroup, archive, snapshotURL)
+	_, err = stmt.Exec(url, title, note, keywords, bGroup, archive, snapshotURL)
 	if err != nil {
 		logger.Error.Fatalln(err)
 	}
-	logger.Info.Println(execResult.RowsAffected())
 }
 
 func Update(database *sql.DB, url, title, note, keywords, bGroup string, id int, archive, s bool, snapshotURL string) {
@@ -38,19 +37,15 @@ func Update(database *sql.DB, url, title, note, keywords, bGroup string, id int,
 		url, title, note, keywords, bGroup = updateCheck(database, url, title, note, keywords, bGroup, id)
 	}
 
-	logger.Info.Printf("url %s, title %s, note %s, keywords %s, bGroup %s, id %d, archive %t, s %t, snapshotURL %s\n", url, title, note, keywords, bGroup, id, archive, s, snapshotURL)
-
 	stmt, err := database.Prepare("UPDATE bookmarks SET url=?, title=?, note=?, keywords=?, bGroup=?, archived=?, snapshotURL=? WHERE id=?;")
 	if err != nil {
 		logger.Error.Fatalln(err)
 	}
 
-	exec, err := stmt.Exec(url, title, note, keywords, bGroup, archive, snapshotURL, id)
+	_, err = stmt.Exec(url, title, note, keywords, bGroup, archive, snapshotURL, id)
 	if err != nil {
 		logger.Error.Fatalln(err)
 	}
-
-	logger.Info.Println(exec.RowsAffected())
 }
 
 func updateCheck(database *sql.DB, url, title, note, keywords, bGroup string, id int) (string, string, string, string, string) {
@@ -84,11 +79,10 @@ func Remove(database *sql.DB, id int) {
 		logger.Error.Fatalln(err)
 	}
 
-	execResult, err := stmt.Exec(id)
+	_, err = stmt.Exec(id)
 	if err != nil {
 		logger.Error.Fatalln(err)
 	}
-	logger.Info.Println(execResult.RowsAffected())
 }
 
 func RemoveAll(database *sql.DB) {
