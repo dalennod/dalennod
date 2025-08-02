7d7278aacc28ea3fcedda68d32b6d0d5fe136185
m.d <49080343+misokt@users.noreply.github.com>
2025-08-01 22:06:19 -0400

    "Spent 3 hours debugging a missing semi-colon"

--- web/static/js/theme-mode.js
+++ web/static/js/theme-mode.js
@@ -7,7 +7,7 @@ const getInitialMode = () => {
 
     const colorScheme = window.matchMedia("(prefers-color-scheme:dark)");
     return colorScheme.matches ? "dark" : "light";
-}
+};
 
 const applyMode = (mode) => {
     const rootElement = document.documentElement;
@@ -16,14 +16,14 @@ const applyMode = (mode) => {
     } else {
         rootElement.removeAttribute("theme-mode");
     }
-}
+};
 
 const modeToggle = () => {
     const currentMode = document.documentElement.getAttribute("theme-mode") === "dark" ? "dark" : "light";
     const nextMode = currentMode === "dark" ? "light" : "dark";
     applyMode(nextMode);
     localStorage.setItem(MODE_KEY, nextMode);
-}
+};
 
 const initThemeMode = (() => {
     const initialMode = getInitialMode();
